3d4b24f0104c2cda94c9ddb69c956a00f26ba0ca
8zipcore <howtofindjayu@gmail.com>
2025-09-23 23:36:41 +0900

    chore: Thread.sleep 제거

--- YoutubeChat/Resource/AppDelegate.swift
+++ YoutubeChat/Resource/AppDelegate.swift
@@ -9,13 +9,7 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
-  
-  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    
-    Thread.sleep(forTimeInterval: 3.0)
-    
     return true
   }
   
@@ -23,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
   
-  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-  }
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
 }
 
